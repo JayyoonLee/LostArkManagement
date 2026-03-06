543ddace40d1a698bb271468d9ee1f955e2d0512
Jayyoon Lee <jayyoon.lee98@gmail.com>
2026-03-07 00:54:18 +0900

    오류수정 Onopen 삭제

--- Code.js
+++ Code.js
@@ -2,18 +2,3 @@
 function onEdit(e){
   handleLevelEdit(e);
 }
-
-function onOpen(e) {
-  const ui = SpreadsheetApp.getUi();
-  ui.showToast('거래소 아이템 가격 업데이트 중...', '잠시만 기다려주세요', -1);
-  try {
-    updateMyCustomItems();
-    ui.showToast('업데이트 완료', '거래소 아이템 가격 업데이트가 완료되었습니다.', 5);
-  } catch (error) {
-    Logger.log('onOpen 함수 실행 중 오류 발생:');
-    Logger.log(error);
-    if (error.stack) {
-      Logger.log(error.stack);
-    }
-  }
-}
